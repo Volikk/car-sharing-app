4ab5ea87a266fa7ff78bac10138fabea9c23ff86
Volikkk <illavolanuk02@gmail.com>
2026-05-19 11:00:12 +0200

    implement JWT token generation and authentication login endpoint

---
+++ src/main/java/com/example/demo/dto/UserLoginResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class UserLoginResponseDto {
+    private String token;
+
+    public UserLoginResponseDto(String token) {
+        this.token = token;
+    }
+}
